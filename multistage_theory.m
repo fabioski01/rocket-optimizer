9cae0810c3bbdf8bea3d901298a99141f08aded8
Fabio Meloni <106022293+fabioski01@users.noreply.github.com>
2024-06-25 11:14:25 +0200

    feat(multistage): create multi-stage theory file for mass ratios optimization depending on mission goals

---
+++ multistage_theory.m
@@ -0,0 +1,11 @@
+%%% Multi-Stage rocket theory
+
+% Goal: Minimization of gross lift-off weight
+
+% 1st order Constraint: Specified mission constraints
+Mu = 400; % Useful Payload mass in Kg
+h = 300; % Target orbit altitude, assuming circular orbit, in Km
+
+% constraints derived from 1st order ones, which are calculated depending
+% on variables
+
