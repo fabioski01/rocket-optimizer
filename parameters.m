86ef20116eaf07781a40ea521608a049df397b30
jancanet <jancanetvidal@gmail.com>
2024-06-26 23:01:08 +0200

    feat(parameters): added some parameters

--- parameters.m
+++ parameters.m
@@ -17,5 +17,10 @@ q = 0.5*1.225*100^2;    % Maximum dynamic pressure [Pa]
 alpha = deg2rad(0.5);   % Angle of attack [deg->rad]
 g0 = 9.81;              % Gravitational acceleration at Earth's surface [m/s^2]
 thrust_1e = 90e3;       % Thrust of one SL engine [N]
+SF = 2;                 % Safety factor
+
 rho = 2550;             % Structural material density [kg/m^3]
+E = 77e9;               % Young modulus [Pa]
+sig_y = 210e6;          % Yield strength [Pa]
+sig_s = 200e6;          % Shear strength [Pa]
 
