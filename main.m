ef427f7199f3fcfd57e54b359f1483ae62d4ebe8
jancanet <jancanetvidal@gmail.com>
2024-06-26 23:04:34 +0200

    feat(main): created main file

---
+++ main.m
@@ -0,0 +1,20 @@
+clc, clear, close all
+
+% Design varibles
+phi = 4;    % Rocket stage diameter [m]
+th = 0.005; % Rocket stage wall thickness [m]
+L = 20;     % Rocket stage length [m]
+n_st = 3;   % Number of stages
+n_e = 9;    % Number of engines (in the 1st stage)
+
+% Define constant parameters
+parameters;
+
+% Objective function
+f = structural_mass(phi,th,L,n_st,n_e);
+
+% Constraints
+g = constraints(phi,th,L,n_st,n_e);
+
+
+
