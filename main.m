342a2d2fb5d5c9e16f091e9d7f5509cee339c4c2
Fabio Meloni <106022293+fabioski01@users.noreply.github.com>
2024-08-05 08:50:02 +0200

    fix(minor changes): updating settings

--- main.m
+++ main.m
@@ -1,11 +1,13 @@
 clc, clear, close all
 
 % Design variables
-phi = [3.6, 3.7, 3.8];    % Rocket stage diameter (3rd, 2nd, 1st) [m]
-th = [0.004, 0.005, 0.006]; % Rocket stage wall thickness (3rd, 2nd, 1st) [m]
+phi = [3.65, 3.7, 3.75];    % Rocket stage diameter (3rd, 2nd, 1st) [m]
+th = [0.0045, 0.005, 0.0059]; % Rocket stage wall thickness (3rd, 2nd, 1st) [m]
 L = [10, 15, 20];     % Rocket stage length (3rd, 2nd, 1st) [m]
 n_st = 3;   % Number of stages
-n_e = 6;   % Number of engines (in the 1st stage)
+n_e = 9;   % Number of engines (in the 1st stage)
+% kick_angle = deg2rad(9);    % Kick angle for starting the gravity turn [deg]
+% kick_time = 30;     % Moment in time when the kick angle is induced to start gravity turn [s] (can also be a percentage of the time span)
 
 % phi = [3.5, 3.5];    % Rocket stage diameter (3rd, 2nd, 1st) [m]
 % th = [0.005, 0.005]; % Rocket stage wall thickness (3rd, 2nd, 1st) [m]
@@ -17,7 +19,7 @@ n_e = 6;   % Number of engines (in the 1st stage)
 parameters;
 
 % Objective function
-f = structural_mass(phi,th,L,n_st,n_e);
+% f = structural_mass(phi,th,L,n_st,n_e);
 
 % Constraints
 [g,h] = constraints(phi,th,L,n_st,n_e);
